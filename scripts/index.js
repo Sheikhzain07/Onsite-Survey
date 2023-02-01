fac0785ff19b27f8ac5ae7f553354bac547afc1f
Sheikhzain07 <sheikhzain459@gmail.com>
2023-02-02 03:08:27 +0530

    working on responsiveness

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,15 @@
-document.getElementById("");
+document.getElementById("form").addEventListener("submit", userDetail);
+
+function userDetail() {
+  event.preventDefault();
+
+  let email = document.getElementById("email").value;
+  let number = document.getElementById("number").value;
+  let code = document.getElementById("code").value;
+  console.log(email);
+  localStorage.setItem("emailKey", email);
+  localStorage.setItem("numberKey", number);
+  localStorage.setItem("codeKey", code);
+  //   <a href="./description/description.html"></a>;
+  window.location.href = "./description/description.html";
+}
