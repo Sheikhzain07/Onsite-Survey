0bc37f5701746279e2c1daf23d32f3034b0c2b43
Sheikhzain07 <sheikhzain459@gmail.com>
2023-02-02 15:38:20 +0530

    onsite survey assignment completed

--- scripts/index.js
+++ scripts/index.js
@@ -6,10 +6,21 @@ function userDetail() {
   let email = document.getElementById("email").value;
   let number = document.getElementById("number").value;
   let code = document.getElementById("code").value;
-  console.log(email);
+  //   console.log(email);
   localStorage.setItem("emailKey", email);
   localStorage.setItem("numberKey", number);
   localStorage.setItem("codeKey", code);
-  //   <a href="./description/description.html"></a>;
-  window.location.href = "./description/description.html";
+
+  if (!email.includes("@") || !email.includes(".com") || email.length < 8) {
+    alert("You have entered an invalid email address");
+  } else if (number.length !== 10) {
+    alert("You have entered an invalid Mobile number");
+  } else if (
+    email.includes("@") &&
+    email.includes(".com") &&
+    email.length > 8 &&
+    number.length === 10
+  ) {
+    window.location.href = "./description/description.html";
+  }
 }
